b60386eb50e73cb8fb5dd32e903639d0f667f05f
Gor92 <gorgrigoryan.1992@gmail.com>
2022-10-16 03:40:07 +0400

    added JSON Reference Loop ignore

--- RestaurantManagement/Program.cs
+++ RestaurantManagement/Program.cs
@@ -16,7 +16,7 @@ using RestaurantManagement.Core.Services.Implementation;
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson(o=>o.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddAuthentication(options =>
